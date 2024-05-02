30b97e3da6367a08ac31b4dbacaa3844f21de0e4
JuniorAlphonse <jalphonse1@ymail.com>
2024-05-02 14:39:03 -0400

    Add clickable button feature that changes background color to light blue

---
+++ PortfolioWebsite.js
@@ -0,0 +1,5 @@
+function onButtonClick() {
+    document.body.style.backgroundColor = "lightblue";
+}
+const button = document.querySelector('button');
+button.addEventListener('click', onButtonClick);
